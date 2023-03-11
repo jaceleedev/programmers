ca34384aceba9841b329c8dfa2a5c8960bdfa3c4
JaceLee <jaceleedev@gmail.com>
2023-03-11 20:23:38 +0900

    Lv.0 몫 구하기 풀이 완료

---
+++ lv.0/6.find-the-quotient.js
@@ -0,0 +1,6 @@
+// 프로그래머스 Lv.0 몫 구하기 문제
+// https://school.programmers.co.kr/learn/courses/30/lessons/120805?language=javascript
+
+function solution(num1, num2) {
+  return Math.floor(num1 / num2)
+}
