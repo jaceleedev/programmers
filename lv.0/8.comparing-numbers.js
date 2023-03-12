2118e7f3017d422968d6c85ea6176c22b98cb972
JaceLee <jaceleedev@gmail.com>
2023-03-12 18:07:52 +0900

    Lv.0 숫자 비교하기 풀이 완료

---
+++ lv.0/8.comparing-numbers.js
@@ -0,0 +1,6 @@
+// 프로그래머스 Lv.0 숫자 비교하기 문제
+// https://school.programmers.co.kr/learn/courses/30/lessons/120807?language=javascript
+
+function solution(num1, num2) {
+  return num1 === num2 ? 1 : -1
+}
