3a4cfc9f589941640f7e8406a9e247d1306ec32b
JaceLee <jaceleedev@gmail.com>
2023-03-12 18:03:35 +0900

    Lv.0 두 수의 나눗셈 풀이 완료

--- lv.0/7.the-division-of-two-numbers.js
+++ lv.0/7.the-division-of-two-numbers.js
@@ -1,5 +1,5 @@
 // 프로그래머스 Lv.0 두 수의 나눗셈 문제
-// https://school.programmers.co.kr/learn/courses/30/lessons/120806
+// https://school.programmers.co.kr/learn/courses/30/lessons/120806?language=javascript
 
 function solution(num1, num2) {
   return Math.floor((num1 / num2) * 1000)
