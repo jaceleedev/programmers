aae01a5f0338ab33eb10939c60788921980b376c
JaceLee <jaceleedev@gmail.com>
2023-03-13 16:31:41 +0900

    Lv.0 배열 두 배 만들기 풀이 완료

---
+++ lv.0/10.double-the-array.js
@@ -0,0 +1,6 @@
+// 프로그래머스 Lv.0 배열 두 배 만들기 문제
+// https://school.programmers.co.kr/learn/courses/30/lessons/120809?language=javascript
+
+function solution(numbers) {
+  return numbers.map((number) => number * 2)
+}
